63974ee8350504d11c80a521373876d637516574
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-01-08 14:39:12 +0500

    Add Repository

---
+++ src/UserApp-Blazor.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using UserApp_Blazor.Domain.Entities;
+
+namespace UserApp_Blazor.Data.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> InsertAsync(User user);
+    Task<User> UpdateAsync(User user);
+    Task<IEnumerable<User>> SelectAllAsync();
+    Task<bool> SaveAsync();
+}
---
+++ src/UserApp-Blazor.Data/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using UserApp_Blazor.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using UserApp_Blazor.Domain.Entities;
+
+namespace UserApp_Blazor.Data.Repositories;
+
+public class UserRepository(AppDbContext context) : IUserRepository
+{
+    public async Task<User> InsertAsync(User user)
+    {
+        return (await context.Users.AddAsync(user)).Entity;
+    }
+
+    public async Task<User> UpdateAsync(User user)
+    {
+        return await Task.FromResult(context.Set<User>().Update(user).Entity);
+    }
+
+
+    public async Task<IEnumerable<User>> SelectAllAsync()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
