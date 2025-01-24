731d85724792b26e6ff7dd13ce3b6fa8b9e8f12f
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-01-24 18:46:55 +0500

    Add WebService

---
+++ src/UserApp-Blazor.Web.Service/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using UserApp_Blazor.Web.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserApp_Blazor.Web.Service.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var apiUrl = configuration["Api:Url"]!;
+        services.AddHttpClient("ApiClient", client =>
+        {
+            client.BaseAddress = new Uri(apiUrl, UriKind.Absolute);
+        });
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
---
+++ src/UserApp-Blazor.Web.Service/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using UserApp_Blazor.Domain.Entities;
+
+namespace UserApp_Blazor.Web.Service.Services;
+
+public class UserService : IUserService
+{
+    public Task<User> CreateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> UpdateAsync(long id, User user)
+    {
+        throw new NotImplementedException();
+    }
+}
