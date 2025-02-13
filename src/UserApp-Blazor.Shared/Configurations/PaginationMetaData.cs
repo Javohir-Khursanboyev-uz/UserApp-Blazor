9d06f135c9c9880932fedb9c7dfaf9ae35a7c736
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-13 23:17:02 +0500

    add

---
+++ src/UserApp-Blazor.Shared/Configurations/PaginationMetaData.cs
@@ -0,0 +1,19 @@
+﻿namespace UserApp_Blazor.Shared.Configurations;
+
+public class PaginationMetaData
+{
+    public PaginationMetaData(int totalCount, PaginationParams @params)
+    {
+        TotalCount = totalCount;
+        PageSize = @params.PageSize;
+        CurrentPage = @params.PageIndex;
+        TotalPages = (int)Math.Ceiling(totalCount / (double)@params.PageSize);
+    }
+
+    public int TotalCount { get; }
+    public int PageSize { get; }
+    public int CurrentPage { get; }
+    public int TotalPages { get; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+}
